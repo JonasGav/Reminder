28a84c9f4c66b10c01b5e7373fc774882ed37895
Gavėnavičius Jonas <j.gavenavicius@gmail.com>
2018-01-21 19:50:05 +0200

    Add project files.

---
+++ Reminder/DatabaseManagement/ControlPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.DatabaseManagement
+{
+    interface ControlPanel
+    {
+        void InsertElement();
+        void CreateTable();
+        void DeleteElement();
+        void DeleteTable();
+    }
+}
